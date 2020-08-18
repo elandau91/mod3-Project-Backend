c8d27f1f0236b3d81cd57dda2c2c025c9535bf5f
elandau91 <eli.b.landau@gmail.com>
2020-08-18 17:29:02 -0400

    edit posts done, carousel working nice

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,11 +10,21 @@ class PostsController < ApplicationController
         render json: post, include: [:likes, :comments]
     end
 
+    def update
+        post = Post.find(params[:id])
+        post.update(post_params)
+    end
+
     def destroy
         post = Post.find(params[:id])
 
         post.destroy
     end
     
+    private
+    
+    def post_params
+        params.require(:post).permit(:content)
+    end
 
 end
