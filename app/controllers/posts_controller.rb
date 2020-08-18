b4aa807d85b3bd22219edef4acf3e52103927bf4
elandau91 <eli.b.landau@gmail.com>
2020-08-18 15:22:39 -0400

    updated routes, updated controllers for delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,17 @@ class PostsController < ApplicationController
         posts = Post.all
         render json: posts, include: [:likes, :comments]
     end
+
+    def show
+        post = Post.find(params[:id])
+        render json: post, include: [:likes, :comments]
+    end
+
+    def destroy
+        post = Post.find(params[:id])
+
+        post.destroy
+    end
     
 
 end
